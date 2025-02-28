b4226763caad80da32b1d41601f5798239dbfbc3
severinnhq <sstierbusiness@gmail.com>
2025-03-01 00:42:44 +0100

    ready

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -281,7 +281,7 @@ export default function HeroSection() {
               variants={testimonialsVariant}
               className="flex flex-col items-center lg:items-start mb-6"
             >
-          <div className="flex items-center w-auto">
+          <div className="flex -space-x-1 sm:-space-x-2 mr-2">
   {/* Profile Images - Made smaller and placed inline */}
   <div className="flex -space-x-2 mr-2">
     <div className="w-8 h-8 rounded-full border-2 border-white overflow-hidden bg-gray-200 z-50">
@@ -337,15 +337,15 @@ export default function HeroSection() {
   </div>
                 
                 {/* Stars - Placed to the right of the images */}
-                <div className="flex">
+                <div className="flex items-center pl-2">
                   {[1, 2, 3, 4, 5].map((star) => (
                     <svg 
-                      key={star} 
-                      xmlns="http://www.w3.org/2000/svg" 
-                      className="h-4 w-4 text-yellow-400" 
-                      viewBox="0 0 20 20" 
-                      fill="currentColor"
-                    >
+                          key={star} 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          className="h-5 w-5 text-yellow-400" 
+                          viewBox="0 0 20 20" 
+                          fill="currentColor"
+                        >
                       <path 
                         d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" 
                       />
