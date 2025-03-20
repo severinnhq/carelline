05f01ddbe6f4de42096013e1076c7916b31cc258
severinnhq <sstierbusiness@gmail.com>
2025-03-20 16:16:01 +0100

    ready

--- app/api/utanvet-checkout/route.ts
+++ app/api/utanvet-checkout/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { MongoClient, ObjectId } from 'mongodb';
-import { Expo } from 'expo-server-sdk';
+
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
