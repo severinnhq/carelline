bc3895a4c5dcb87930f2d020666ed74381b8f597
severinnhq <sstierbusiness@gmail.com>
2025-06-01 16:35:28 +0200

    ready

--- components/WhiteHeader.tsx
+++ components/WhiteHeader.tsx
@@ -22,7 +22,7 @@ export function WhiteHeader({ onCartClick, cartItems }: WhiteHeaderProps) {
   const [isTransitioning, setIsTransitioning] = useState(false)
 
   const bannerText = "NYÁR INDÍTÓ AKCIÓ CSAK JÚNIUS 4-IG!!"
-  const bankText = `BANKKÁRTYÁS FIZETÉS ESETÉN -20% A "NYAR10" KÓDDAL`
+  const bankText = `BANKKÁRTYÁS FIZETÉS ESETÉN -20% A "NYAR20" KÓDDAL`
 
   const cartItemsCount = cartItems.reduce((total, item) => total + item.quantity, 0)
 
