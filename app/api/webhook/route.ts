e7914e93a6e69a8155095333dea9a2c5b61d6343
severinnhq <sstierbusiness@gmail.com>
2025-07-04 21:30:39 +0200

    webhook fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
 
 interface OrderItem {
