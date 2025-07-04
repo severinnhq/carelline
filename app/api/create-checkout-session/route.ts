8974812efdb17aae3f7e2dd3a86a41c49492fa5b
severinnhq <sstierbusiness@gmail.com>
2025-07-04 21:28:42 +0200

    stripe error fix

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 // Make sure your API key is correctly set in your environment variables
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
 
 interface CartItem {
